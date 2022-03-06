b9ff956e27f8206bacdb627eca610ee1a0a6757a
Mrq9rawi <moh.abdullah02@gmail.com>
2022-03-05 22:59:02 -0500

    project created and landing is done

---
+++ main.js
@@ -0,0 +1,7 @@
+// const continueLink = document.querySelector(".landing a");
+
+// continueLink.addEventListener("click", (e) => {
+// 	setTimeout(() => {
+// 		e.target.parentElement.remove();
+// 	}, 100);
+// });
